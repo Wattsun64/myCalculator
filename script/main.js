054fd994b29e0d510e315ff591de862b446262b1
Wattsun64 <ecochra@gmail.com>
2017-04-09 16:35:05 -0400

    Created IIFE in main.js file

--- script/main.js
+++ script/main.js
@@ -1,63 +1,65 @@
 // Main Variables
+(function(){
+    var display = document.getElementById('display'),
+        out = document.getElementById('out'),
+        num = document.querySelectorAll('.number'),
+        clear = document.getElementById('clear'),
+        op = document.querySelectorAll('.operator'),
+        equal = document.getElementById('equal'),
+        i;
 
-var display = document.getElementById('display'),
-    out = document.getElementById('out'),
-    num = document.querySelectorAll('.number'),
-    clear = document.getElementById('clear'),
-    op = document.querySelectorAll('.operator'),
-    equal = document.getElementById('equal'),
-    i;
-
-clear.onclick = function() {
-    out.value = '';
-    display.innerText = '';
-}
+    clear.onclick = function() {
+        out.value = '';
+        display.innerText = '';
+    }
 
 // Input numbers
 
-for ( i = 0; i < num.length; i++) {
-    num[i].onclick = function() {
-        out.value += this.value;
+    for ( i = 0; i < num.length; i++) {
+        num[i].onclick = function() {
+            out.value += this.value;
+        }
     }
-}
 
 // Retrieve first number variable, clear out value and add event listner in order to retrieve numTwo value
 
-for ( i = 0; i < op.length; i++ ) {
-    op[i].onclick = function() {
-        var numOne = parseInt(out.value),
-            opVal = this.value,
-            numTwo;
+    for ( i = 0; i < op.length; i++ ) {
+        op[i].onclick = function() {
+            var numOne = parseInt(out.value),
+                opVal = this.value,
+                numTwo;
 
-        out.value = '';
+            out.value = '';
 
-        display.innerText = numOne + ' ' + opVal;
+            display.innerText = numOne + ' ' + opVal;
 
-        for ( i = 0; i < num.length; i++ ) {
-            num[i].addEventListener('click', function() {
-                numTwo = parseInt(out.value);
-                sortVal(opVal,numOne,numTwo);
-            })
+            for ( i = 0; i < num.length; i++ ) {
+                num[i].addEventListener('click', function() {
+                    numTwo = parseInt(out.value);
+                    sortVal(opVal,numOne,numTwo);
+                })
+            }
         }
     }
-}
 
-function sortVal(o,i,t) {
-    var total;
+    function sortVal(o,i,t) {
+        var total;
 
-    if ( o === '+' ) {
-        total = i + t;
-    } else if ( o === '-' ) {
-        total = i - t;
-    } else if ( o === 'x' ) {
-        total = i * t;
-    } else if ( o === '÷' ) {
-        total = i / t;
+        if ( o === '+' ) {
+            total = i + t;
+        } else if ( o === '-' ) {
+            total = i - t;
+        } else if ( o === 'x' ) {
+            total = i * t;
+        } else if ( o === '÷' ) {
+            total = i / t;
+        }
+
+        equal.onclick = function() {
+            out.value = total;
+            display.innerText = i + ' ' + o + ' ' + t + ' = ' + total;
+        }
     }
+}())
 
-    equal.onclick = function() {
-        out.value = total;
-        display.innerText = i + ' ' + o + ' ' + t + ' = ' + total;
-    }
-}
 
